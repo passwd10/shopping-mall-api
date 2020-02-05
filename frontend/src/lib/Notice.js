b19a163b4e4e386f232df3153ad224532d255c19
passwd10 <parkinseo94@naver.com>
2020-02-05 16:15:39 +0900

    Clean push

--- frontend/src/lib/Notice.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styeld from 'styled-components';
-
-const ModifyNotice = styeld.span`
-    padding-left: 10px;
-`;
-
-export default ModifyNotice;
--- frontend/src/services/config.js
+++
@@ -1,4 +0,0 @@
-const SERVER_URL = 'http://localhost:8080';
-export const API_SERVER_URL = 'http://localhost:3000';
-
-export default SERVER_URL;
